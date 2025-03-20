c4cd00f07e874ac97458f614bd89708b5a89e302
Yasmin Belo <beloyasmin93@gmail.com>
2025-03-20 20:07:22 -0300

    atualização

---
+++ lib/myapp.dart
@@ -0,0 +1,11 @@
+import 'package:exercicio_1/homepage.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: homepage());
+  }
+}
